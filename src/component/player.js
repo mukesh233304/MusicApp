fea9aa923960e2d1b18936007464049ce1b097ac
Mukesh Sonkar <mukeshsonkarbhu@gmail.com>
2024-02-22 23:51:41 +0530

    initial commit

---
+++ src/component/player.js
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import qala from "../assets/qala.mp3";
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
+//import { FaCalendar } from 'react-icons/fa'
+//import { AiIconName, BiIconName } from 'react-icons';
+import { IconContext } from "react-icons";
+
+export default function Player() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [time, setTime] = useState({
+    min: "",
+    sec: ""
+  });
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sec: ""
+  });
+
+  const [seconds, setSeconds] = useState();
+
+  const [play, { pause, duration, sound }] = useSound(qala);
+
+  useEffect(() => {
+    if (duration) {
+      const sec = duration / 1000;
+      const min = Math.floor(sec / 60);
+      const secRemain = Math.floor(sec % 60);
+      setTime({
+        min: min,
+        sec: secRemain
+      });
+    }
+  }, [isPlaying, duration]); // Include 'duration' in the dependency array
+  
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([]));
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+          min,
+          sec
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
+
+  const playingButton = () => {
+    if (isPlaying) {
+      pause();
+      setIsPlaying(false);
+    } else {
+      play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <div className="component">
+      <h2>Playing Now</h2>
+      <img className="musicCover" src="https://fastly.picsum.photos/id/596/200/200.jpg?hmac=TiMsstBNF6YKq9Gn7QGsihITEUcv_O8QuTXEVR3T6GA" alt="https://fastly.picsum.photos/id/596/200/200.jpg?hmac=TiMsstBNF6YKq9Gn7QGsihITEUcv_O8QuTXEVR3T6GA" />
+      <div>
+        <h3 className="title">Rubaiyyan</h3>
+        <p className="subTitle">Qala</p>
+      </div>
+      <div>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
+      </div>
+      <div>
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipPrevious />
+          </IconContext.Provider>
+        </button>
+        {!isPlaying ? (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPlayCircle />
+            </IconContext.Provider>
+          </button>
+        ) : (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPauseCircle />
+            </IconContext.Provider>
+          </button>
+        )}
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipNext />
+          </IconContext.Provider>
+        </button>
+      </div>
+    </div>
+  );
+}
